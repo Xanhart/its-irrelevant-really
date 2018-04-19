2d682a67721d735271fddafa0a4cba293e3c7912
Mike Garuccio <mgaruccio@gmail.com>
2018-04-18 23:52:09 -0400

    attempt at getting animals on the main page

---
+++ ScavengerHunt/Models/AnimalModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScavengerHunt.Models
+{
+    public class Animal
+    {
+        public int AnimalID { get; set; }
+        public string AnimalName { get; set; }
+        public string AnimalImageLocation { get; set; }
+    }
+}
--- ScavengerHunt/Models/IdentityModels.cs
+++ ScavengerHunt/Models/IdentityModels.cs
@@ -31,5 +31,9 @@ namespace ScavengerHunt.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<ScavengerHunt.Models.Park> Parks { get; set; }
+
+        public System.Data.Entity.DbSet<ScavengerHunt.Models.Animal> Animals { get; set; }
     }
 }
