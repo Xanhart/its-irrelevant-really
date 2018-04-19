6bfc388f3b5b0736fbd003df1cbb047f7f6df85c
Mike Garuccio <mgaruccio@gmail.com>
2018-04-19 19:52:21 -0400

    got the home page to show the animals

--- ScavengerHunt/Controllers/HomeController.cs
+++ ScavengerHunt/Controllers/HomeController.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using ScavengerHunt.Models;
 
 namespace ScavengerHunt.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Animals.ToList());
         }
     }
 }
